df829632300417b1023b429eef062b6504543970
Marta Klachko <marta.klachko@gmail.com>
2018-01-13 18:00:17 +0200

    added sorting into Burrows-Wheeler tranformation

--- lesson4/src/Main.java
+++ lesson4/src/Main.java
@@ -28,72 +28,54 @@ public class Main {
         System.out.println(modifiedText);
     }
 
-    static void toRight(String text) {
+    static void rightJustify(String text) {
         StringBuilder modifiedText = new StringBuilder(text).reverse();
-        while (modifiedText.length() < 197) {
+        while (modifiedText.length() < 197) {//197 is size of the console
             modifiedText.append(" ");
         }
         System.out.println(modifiedText.reverse());
+
+
     }
 
 
-    static void BurrowsWheelerTransform(String text) {
-        char[][] words = new char[text.length()][text.length()];
-        char[] textToArray = new char[text.length()];
-        for (int i = 0; i < textToArray.length; i++) {
-            textToArray[i] = text.charAt(i);
+    static void BurrowsWheelerTransform() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter text to transform:");
+        String text = sc.next();
+        String[] transformations = new String[text.length()];
+        System.out.println("transformations:");
+        //make transformations with a word
+        for (int i = 0; i < transformations.length; i++) {
+            transformations[i] = text.substring(1, transformations.length) + text.substring(0, 1);
+            text = transformations[i];
+            System.out.println(transformations[i]);
         }
-        for (int i = 0; i < textToArray.length; i++) {
-
-            char[] transformedArray = new char[text.length()];
-
-            transformedArray[transformedArray.length - 1] = textToArray[0];
-
-            for (int j = 0; j < textToArray.length - 1; j++) {
-                transformedArray[j] = textToArray[j + 1];
+        //sorting these transformations
+        for (int i = transformations.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (transformations[j].compareTo(transformations[j + 1]) > 0) {
+                    String tmp = transformations[j];
+                    transformations[j] = transformations[j + 1];
+                    transformations[j + 1] = tmp;
+                }
             }
-            words[i] = transformedArray;
-
-            textToArray = transformedArray;
-
-            for (int k = 0; k < transformedArray.length; k++) {
-                System.out.print(transformedArray[k]);
-
-            }
-            System.out.println();
-
-
         }
-//        char[][] sorted = new char[4][4];
-//        char[] min = words[0];
-//        for (int i = 0; i < 4; i++) {
-//            for (int j = 0; j < 4; j++) {
-//                if (words[i][j] < min[j]) {
-//                    min = words[i];
-//                    sorted[i] = min;
-//                }
-//
-//            }
-//
-//        }
-//        System.out.println("!!!!!!!!!!!");
-//        for (int i = 0; i < 4; i++) {
-//            for (int j = 0; j < 4; j++) {
-//                System.out.print(sorted[i][j]);
-//            }
-//            System.out.println();
-//        }
-//        System.out.println();
-//
+        System.out.println("sorted in alphabetical order:");
+        for (int i = 0; i < transformations.length; i++) {
+            System.out.println(transformations[i]);
+        }
 
+        //taking last letters of sorted transformations
+        StringBuilder lastLetters = new StringBuilder();
+
+        for (int i = 0; i < transformations.length; i++) {
+            lastLetters.append(transformations[i].charAt(transformations[i].length() - 1));
+        }
         System.out.println("last letters:");
-        for (char[] word : words
-                ) {
+        System.out.println(lastLetters);
 
-            System.out.print(word[3]);
-
-        }
-
+        sc.close();
 
     }
 
@@ -105,10 +87,10 @@ public class Main {
 
             String str = sc.nextLine();
             deleteRepeatableSymbols(str);
-            toRight(str);
+            rightJustify(str);
 
         }
-      //  BurrowsWheelerTransform("java");
+         BurrowsWheelerTransform();
 
     }
 
